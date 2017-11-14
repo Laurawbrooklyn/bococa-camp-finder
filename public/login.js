02205a2c5d6b20db7dee1724e242e8b5f65a110d
Laura Wallace <lauranestwallace@gmail.com>
2017-11-14 13:59:45 -0500

    changes to add test button

--- public/login.js
+++ public/login.js
@@ -1,30 +1,37 @@
 $(document).ready(function() {
 
-$("#login-form").click(function(event) {
-	event.preventDefault();
-
-			var loginData = {
-				username: $( "#username-input" ).val(),
-				password: $("#password-input").val(),
-			}
-			console.log(loginData);
-			$.ajax({
-				url: `/api/auth/login`,
-				type: "POST",
-				data: JSON.stringify(loginData),
-				contentType: "application/json; charset=utf-8",
-				dataType: "json",
-				success: function(data){
-					var token = data.authToken
-					localStorage.setItem('token', token);
-					console.log(token);
-          window.location.href = "/add-a-camp.html";
-				},
-				error: function(errorData){
-					console.log("err");
-					console.log(errorData);
-				},
-			});
-		});
-
+	$("#login-form").click(function(event) {
+		event.preventDefault();
+		login()
 	});
+
+});
+$("#login-test").click(function(event) {
+	login('testusername', 'test1234')
+});
+
+});
+function login(username, password) {
+	var loginData = {
+		username: username || $( "#username-input" ).val(),
+		password: password || $("#password-input").val(),
+	}
+	console.log(loginData);
+	$.ajax({
+		url: `/api/auth/login`,
+		type: "POST",
+		data: JSON.stringify(loginData),
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		success: function(data){
+			var token = data.authToken
+			localStorage.setItem('token', token);
+			console.log(token);
+			window.location.href = "/add-a-camp.html";
+		},
+		error: function(errorData){
+			console.log("err");
+			console.log(errorData);
+		},
+	});
+}
